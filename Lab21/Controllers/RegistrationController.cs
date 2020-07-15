25fb5cdb1853c811164ecccc2b7068d70fb74990
kyle-nedbal <kylenedbal88@gmail.com>
2020-07-15 18:16:09 -0400

    MVC basics

---
+++ Lab21/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab21.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab21.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Registration registration)
+        {
+            // Save to a database??? 
+            // Typically you would do something with this...
+            // But for now we will just display the results
+
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+    }
+}
---
+++ Lab21/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21.Models
+{
+    public class Registration
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
